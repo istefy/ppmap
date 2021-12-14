d5b13053b947d4669810552360004108835fda4d
istefy <stefaaan@gmail.com>
2021-12-15 01:29:32 +0300

    Update ppmap.go

--- ppmap.go
+++ ppmap.go
@@ -141,8 +141,8 @@ dMP     dMP     dMP dMP dMP dMP dMP dMP            @kleiton0x7e
 
 	    opts := append(chromedp.DefaultExecAllocatorOptions[:],
 		//uncomment the following lines to setup a proxy
-		chromedp.ProxyServer("localhost:8080"),
-		chromedp.Flag("ignore-certificate-errors", true),
+		//chromedp.ProxyServer("localhost:8080"),
+		//chromedp.Flag("ignore-certificate-errors", true),
 		chromedp.UserAgent(useragents[n]),
 	    )
 	    ctx, cancel := chromedp.NewExecAllocator(context.Background(), opts...)
@@ -150,7 +150,7 @@ dMP     dMP     dMP dMP dMP dMP dMP dMP            @kleiton0x7e
 	    ctx, cancel = chromedp.NewContext(
 		ctx,
 		// uncomment the next line to see the CDP messages
-		chromedp.WithDebugf(log.Printf),
+		//chromedp.WithDebugf(log.Printf),
 	    )
 	    defer cancel()
 
